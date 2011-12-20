65033a77b11621a8d37f2cee5212f8dfd74b7d95
Christian Cantrell <christian.cantrell@gmail.com>
2011-12-20 14:27:35 -0500

    Added Firefox support.

--- javascript/spinner.js
+++ javascript/spinner.js
@@ -1,10 +1,11 @@
 var Spinner = {
     SPINNER_ID: '_spinner',
+    prefix: (navigator.userAgent.indexOf('WebKit') != -1) ? 'webkit' : 'moz',
     getSpinner: function(size, numberOfBars, color) {
         if (document.getElementById(this.SPINNER_ID) == null) {
             var style = document.createElement('style');
             style.setAttribute('id', this.SPINNER_ID);
-            style.innerHTML = '@-webkit-keyframes fade {from {opacity: 1;} to {opacity: 0.25;}}';
+            style.innerHTML = '@-'+this.prefix+'-keyframes fade {from {opacity: 1;} to {opacity: 0.25;}}';
             document.getElementsByTagName('head')[0].appendChild(style);
         }
         var spinner = document.createElement('div');
@@ -24,12 +25,12 @@ var Spinner = {
             bar.style.position = 'absolute';
             bar.style.left = '44.5%';
             bar.style.top = '37%';
-            bar.style.opacity = '0';
-            bar.style['-webkit-border-radius'] = '50px';
-            bar.style['-webkit-box-shadow'] = '0 0 3px rgba(0,0,0,0.2)';
-            bar.style['-webkit-animation'] = 'fade 1s linear infinite';
-            bar.style['-webkit-transform'] = 'rotate('+rotation+'deg) translate(0, -142%)';
-            bar.style['-webkit-animation-delay'] = animationDelay + 's';
+            bar.style.opacity = '1';
+            bar.style.setProperty('-'+this.prefix+'-border-radius', '50px', null);
+            bar.style.setProperty('-'+this.prefix+'-box-shadow', '0 0 3px rgba(0,0,0,0.2)', null);
+            bar.style.setProperty('-'+this.prefix+'-animation', 'fade 1s linear infinite', null);
+            bar.style.setProperty('-'+this.prefix+'-transform', 'rotate('+rotation+'deg) translate(0, -142%)', null);
+            bar.style.setProperty('-'+this.prefix+'-animation-delay', animationDelay + 's', null);
             rotation += rotateBy;
             animationDelay -= frameRate;
         }
